e55ba330fee935629417d08b1482388d35703e7e
IliasOmari <ilias.omari@student.ehb.be>
2023-11-26 14:29:21 +0100

    installed react router dom + created a rout

--- Login/src/main.jsx
+++ Login/src/main.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import LoginForm from "./LoginForm.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginForm />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LoginForm />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
